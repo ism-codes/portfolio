3c144a5f19853e23af498e05530f05a0a42d800e
ism-codes <141295333+ism-codes@users.noreply.github.com>
2023-09-27 08:30:48 -0700

    Removed Starter Template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { CoreNavComponent } from './core-components/core-nav/core-nav.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['../demo-styling.css']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = 'Ismael Khan Portfolio';
 }
