db71126a9b8de79637a31091e32729cc270f9b16
ism-codes <141295333+ism-codes@users.noreply.github.com>
2024-03-27 21:36:35 -0700

    Update basic.cy.ts

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -3,12 +3,4 @@ describe('empty spec', () => {
     cy.visit('/')
   })
 
-
-  it('renders the image', () => {
-    cy.get('img')
-    .should('be.visible')
-    .and(($img) => {
-      expect($img[0].naturalWidth).to.be.greaterThan(0);
-    })
-  })
 })
